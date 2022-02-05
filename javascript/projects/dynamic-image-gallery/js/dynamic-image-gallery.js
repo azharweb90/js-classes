727f911a8e4cda69220cb79c942c305253ed4f9d
Mohammed Azhar Uddin <azharweb90@gmail.com>
2022-02-05 17:12:10 +0530

    this concept added

--- javascript/projects/dynamic-image-gallery/js/dynamic-image-gallery.js
+++ javascript/projects/dynamic-image-gallery/js/dynamic-image-gallery.js
@@ -7,8 +7,15 @@
 
 //gallery source
 
-//Module Pattern: Module pattern is a way to encapsulate logic in a file both public and private methods.
+//Q: What is Module Pattern ? 
+//A: Module pattern is a way to encapsulate logic in a file both public and private methods and variables. It helps in protecting pieces from leaking into the global scope and accidentally collliding with another developer's interface.With this pattern, only a public API is returned, keeping everything else within the closure private.
+
+// The Module Pattern approach is not only incredibly powerful, but incredibly simple. With very little code, you can effectively namespace related methods and properties, organizing entire modules of code in a way that both minimizes global scope pollution and creates privacy
+
+
 var DIG = (function(){
+
+    //dig variable is the private variable
     const dig = {
         formSelector:'',
         formInputSelector:'',
@@ -59,3 +66,4 @@ var DIG = (function(){
         source);
     };
 })();
+
